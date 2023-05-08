103d46bbf5e60835b4883e0052a6c01a12bd671e
SSzSun <jaruphat.sun@gmail.com>
2023-05-08 17:44:22 +0700

    Begin
    React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 function App() {
   return (
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/SSzSun"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
     </div>
---
+++ src/js/ChatlistBox.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ChatlistBox() {
+  return (
+    <div>ChatlistBox</div>
+  )
+}
