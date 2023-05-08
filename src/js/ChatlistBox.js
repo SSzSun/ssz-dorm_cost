7f52e1bd7678ab58e5f1b86f1aa8fb16d7161cc9
SSzSun <jaruphat.sun@gmail.com>
2023-05-08 18:03:44 +0700

    Update ChatlistBox.js

--- src/js/ChatlistBox.js
+++ src/js/ChatlistBox.js
@@ -1,6 +1,16 @@
-import React from
+import React from 'react'
 export default function ChatlistBox() {
   return (
     <div>ChatlistBox</div>
   )
+}
+
+import React, { Component } from 'react'
+
+export default class ChatlistBox extends Component {
+  render() {
+    return (
+      <div>ChatlistBox</div>
+    )
+  }
 }
